d5c54aa9750f74415e31ada6a38de91f5bca691f
player-ime <190986916+player-ime@users.noreply.github.com>
2024-12-22 00:14:35 +0900

    v0.0.15

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "TeamRocket-Game_Rocket-0.0.14";
+const cacheName = "TeamRocket-Game_Rocket-0.0.15";
 const contentToCache = [
     "Build/app-01.loader.js",
     "Build/app-01.framework.js.unityweb",
